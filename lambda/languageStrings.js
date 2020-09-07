35568517a469650e28e6a0266c585fa1a85ca81b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-07 07:25:41 +0000

    updated lambda/languageStrings.js.

--- lambda/languageStrings.js
+++ lambda/languageStrings.js
@@ -8,21 +8,19 @@
 module.exports = {
     en: {
         translation: {
-            SKILL_NAME: 'Space Facts',
-            GET_FACT_MESSAGE: 'Here\'s your fact: ',
-            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
+            SKILL_NAME: 'India Facts',
+            GET_FACT_MESSAGE: 'Here is a fact for you: ',
+            HELP_MESSAGE: 'You can say tell me a fact, or, you can say exit... What can I help you with?',
             HELP_REPROMPT: 'What can I help you with?',
-            FALLBACK_MESSAGE: 'The Space Facts skill can\'t help you with that.  It can help you discover facts about space if you say tell me a space fact. What can I help you with?',
+            FALLBACK_MESSAGE: 'The India Facts skill can\'t help you with that.  It can help you discover facts about space if you say tell me a space fact. What can I help you with?',
             FALLBACK_REPROMPT: 'What can I help you with?',
             ERROR_MESSAGE: 'Sorry, an error occurred.',
             STOP_MESSAGE: 'Goodbye!',
             FACTS:
             [
-                'A year on Mercury is just 88 days long.',
-                'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-                'On Mars, the Sun appears about half the size as it does on Earth.',
-                'Jupiter has the shortest day of all the planets.',
-                'The Sun is an almost perfect sphere.',
+                'Independence day is on fifteenth august',
+                'India is a subcontinent',
+                'India is the seventh largest country in the world.',
             ],
         }
     },
